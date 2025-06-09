9ecdcd155dc123b935006697cf57f045ba71f9d6
TRProjects98 <reistiago64@gmail.com>
2025-06-09 08:37:11 +0100

    Costume image

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,14 +11,14 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#000000',
     icons: [
       {
-        src: '/next.svg',
+        src: '/head.png',
         sizes: '192x192',
-        type: 'image/svg',
+        type: 'image/png',
       },
       {
-        src: '/next.svg',
+        src: '/head-png',
         sizes: '512x512',
-        type: 'image/svg',
+        type: 'image/png',
       },
     ],
   }
