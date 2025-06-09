cc5c2163cc0130d33ce548328ce1d52b0554cbfb
TRProjects98 <reistiago64@gmail.com>
2025-06-09 08:31:24 +0100

    Deploy test

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
    <div className="flex items-center justify-center h-screen">
-    <h1>My first pwa</h1>
+    <h1>My first pwa test</h1>
    </div>
   );
 }
