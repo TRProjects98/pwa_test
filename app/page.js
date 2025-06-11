cd4eea42badbc2d72eaedc59e657ae102b0e8791
Tiago Reis <reistiago@ua.pt>
2025-06-11 12:42:41 +0100

    chore: add console log for debugging in Page component

--- app/page.js
+++ app/page.js
@@ -3,6 +3,8 @@ import PushNotificationManager from './components/PushNotificationManager'
 import InstallPrompt from './components/InstallPrompt'
  
 export default function Page() {
+
+  console.log('Hello from MAC')
   return (
     <div className='flex flex-col gap-4 h-screen justify-center items-center'>
       <PushNotificationManager />
